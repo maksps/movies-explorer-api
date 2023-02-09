e7594c835a50d720c6e0df81602dda41304d30e6
Pavel Maksimov <maksps@yandex.ru>
2023-02-09 22:05:58 +0400

    создан функционал сервера

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const { celebrate, Joi, errors } = require('celebrate');
 // const movies = require('./routes/movies');
 // const NotFoundError = require('./errors/NotFoundError');
 // const auth = require('./middlewares/auth');
+const errorHandler = require('./middlewares/errorHendler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 // const {
 //   createUser, login,
@@ -25,39 +26,9 @@ mongoose.connect('mongodb://localhost:27017/moviesdb', {
 });
 
 app.use(express.json());
-
 app.use(cors());
 app.use(requestLogger);
-// app.post('/signin', celebrate({
-//   body: Joi.object().keys({
-//     email: Joi.string().required().email(),
-//     password: Joi.string().required().min(8),
-//   }),
-// }), login);
-
-// app.post('/signup', celebrate({
-//   body: Joi.object().keys({
-//     email: Joi.string().required().email(),
-//     password: Joi.string().required(),
-//     name: Joi.string().min(2).max(30),
-//   }).unknown(true),
-// }), createUser);
-// app.use(auth);
-// app.use('/movies', movies);
-// app.use('/users', users);
-
-// app.use('*', (req, res, next) => next(new NotFoundError('Неверный URL')));
 app.use(router);
 app.use(errorLogger);
 app.use(errors());
-app.use((err, req, res, next) => {
-  const { statusCode = 500, message } = err;
-  res
-    .status(statusCode)
-    .send({
-      message: statusCode === 500
-        ? 'На сервере произошла ошибка!'
-        : message,
-    });
-  next();
-});
+app.use(errorHandler);
